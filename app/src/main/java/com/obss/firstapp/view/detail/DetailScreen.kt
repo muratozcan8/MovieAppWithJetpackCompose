e40933eee3b9880082c3e9eddebc363c0eff682c
Murat Özcan <102511241+muratozcan8@users.noreply.github.com>
2024-08-22 11:00:28 +0300

    adjusted adding to favorities and removing from favorities movies

--- app/src/main/java/com/obss/firstapp/view/detail/DetailScreen.kt
+++ app/src/main/java/com/obss/firstapp/view/detail/DetailScreen.kt
@@ -1,5 +1,7 @@
 package com.obss.firstapp.view.detail
 
+import android.content.Context
+import android.widget.Toast
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
@@ -30,6 +32,8 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -37,6 +41,7 @@ import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
@@ -50,34 +55,41 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.viewModelScope
 import androidx.navigation.NavController
 import coil.compose.AsyncImage
 import com.google.accompanist.pager.HorizontalPager
 import com.obss.firstapp.R
+import com.obss.firstapp.data.local.FavoriteMovie
 import com.obss.firstapp.data.model.movie.Movie
+import com.obss.firstapp.data.model.movieDetail.MovieDetail
 import com.obss.firstapp.utils.Constants.IMAGE_BASE_URL
 import com.obss.firstapp.utils.ext.roundToSingleDecimal
+import kotlinx.coroutines.launch
 
 @Composable
 fun DetailScreen(
     navController: NavController,
     movieId: Int,
-    homeViewModel: DetailViewModel = hiltViewModel(),
+    detailViewModel: DetailViewModel = hiltViewModel(),
 ) {
-    homeViewModel.getMovieDetails(movieId)
-    homeViewModel.getMovieImages(movieId)
-    homeViewModel.getMovieCasts(movieId)
-    homeViewModel.getRecommendationMovies(movieId)
-    homeViewModel.getReviews(movieId)
-    val movie = homeViewModel.movie.collectAsState().value
-    val movieImages = homeViewModel.movieImages.collectAsState().value
-    val movieCasts = homeViewModel.movieCasts.collectAsState().value
-    val recommendationMovies = homeViewModel.recommendationMovies.collectAsState().value
-    val reviews = homeViewModel.reviews.collectAsState().value
-    val favoriteMovie = homeViewModel.favoriteMovie.collectAsState().value
-    val isLoadings = homeViewModel.isLoadings.collectAsState().value
-    val videos = homeViewModel.videos.collectAsState().value
-    val errorMessage = homeViewModel.errorMessage.collectAsState().value
+    val context = LocalContext.current
+
+    detailViewModel.getMovieDetails(movieId)
+    detailViewModel.getMovieImages(movieId)
+    detailViewModel.getMovieCasts(movieId)
+    detailViewModel.getRecommendationMovies(movieId)
+    detailViewModel.getReviews(movieId)
+    detailViewModel.getFavoriteMovie(movieId)
+
+    val movie = detailViewModel.movie.collectAsState().value
+    val movieImages = detailViewModel.movieImages.collectAsState().value
+    val movieCasts = detailViewModel.movieCasts.collectAsState().value
+    val recommendationMovies = detailViewModel.recommendationMovies.collectAsState().value
+    val reviews = detailViewModel.reviews.collectAsState().value
+    val isLoadings = detailViewModel.isLoadings.collectAsState().value
+    val videos = detailViewModel.videos.collectAsState().value
+    val errorMessage = detailViewModel.errorMessage.collectAsState().value
 
     Box(
         modifier =
@@ -154,13 +166,17 @@ fun DetailScreen(
                         }
 
                         Spacer(modifier = Modifier.weight(1f))
+                        if (movie != null) {
+                            val isFavorite = remember { mutableStateOf(movie.isFavorite ?: false) }
 
-                        IconButton(onClick = { onFavClick(movieId) }) {
-                            if (movie != null) {
+                            IconButton(onClick = {
+                                onFavClick(movieId, movie, detailViewModel, context)
+                                isFavorite.value = !isFavorite.value
+                            }) {
                                 Icon(
                                     painter =
                                         painterResource(
-                                            id = if (movie.isFavorite) R.drawable.favorite_24 else R.drawable.favorite_border_24,
+                                            id = if (isFavorite.value) R.drawable.favorite_24 else R.drawable.favorite_border_24,
                                         ),
                                     contentDescription = null,
                                     tint = Color.White,
@@ -408,6 +424,7 @@ fun DetailScreen(
     }
 }
 
+@Suppress("ktlint:standard:function-naming")
 @Composable
 fun MovieGridItem(
     movie: Movie,
@@ -498,5 +515,35 @@ fun onBackClick(navController: NavController) {
     navController.popBackStack()
 }
 
-fun onFavClick(movieId: Int) {
+fun onFavClick(
+    movieId: Int,
+    movie: MovieDetail?,
+    viewModel: DetailViewModel,
+    context: Context,
+) {
+    if (movie?.isFavorite == true) {
+        movie.id?.let { viewModel.getFavoriteMovie(it) }
+        viewModel.viewModelScope.launch {
+            viewModel.favoriteMovie.collect {
+                if (it != null) {
+                    viewModel.removeFavoriteMovie(it)
+                    movie.isFavorite = false
+                }
+            }
+        }
+        showCustomToast(context, "Removed from favorites")
+    } else {
+        movie?.isFavorite = true
+        viewModel.addFavoriteMovie(
+            favoriteMovie = FavoriteMovie(0, movieId, movie?.title.toString(), movie?.posterPath.toString(), movie?.voteAverage),
+        )
+        showCustomToast(context, "Added to favorites")
+    }
+}
+
+fun showCustomToast(
+    context: Context,
+    message: String,
+) {
+    Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
 }
